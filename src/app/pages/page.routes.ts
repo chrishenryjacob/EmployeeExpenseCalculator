9f806b5e3576a2159e44f8b853085a68949dd7d8
Christopher Henry Jacob <christopherhenryjacob@gmail.com>
2023-08-10 18:24:50 +0400

    employee routes added

--- src/app/pages/page.routes.ts
+++ src/app/pages/page.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from "@angular/router";
 
-export const pageRoutes: Routes = []
+export const pageRoutes: Routes = [
+    {
+        path: 'employee',
+        title: 'Employee',
+        children: [
+            {
+                path: '',
+                loadComponent: () => import('./employee/employee.component')
+                    .then((c) => c.EmployeeComponent)
+            },
+            {
+                path: 'add',
+                loadComponent: () => import('./employee/employee-form/employee-form.component')
+                    .then(c => c.EmployeeFormComponent)
+            },
+            {
+                path: 'edit/:id',
+                loadComponent: () => import('./employee/employee-form/employee-form.component')
+                    .then(c => c.EmployeeFormComponent)
+            }
+        ]
+    }
+]
