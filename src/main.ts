c0f8433bc0de3b04d491b4100878f246131e1821
Christopher Henry Jacob <christopherhenryjacob@gmail.com>
2023-08-10 16:37:47 +0400

    changed project to standalone

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,16 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideRouter, withRouterConfig } from "@angular/router";
 
-import { AppModule } from './app/app.module';
+import { AppComponent } from './app/app.component';
+import { appRoutes } from './app/app.routes';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideAnimations(),
+    provideRouter(
+      appRoutes,
+      withRouterConfig({ onSameUrlNavigation: 'reload' })
+    )
+  ],
+}).catch((err) => console.error(err));
