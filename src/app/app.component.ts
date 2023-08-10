fc5e48567798b2dd84599e960923808534ef5bbf
Christopher Henry Jacob <christopherhenryjacob@gmail.com>
2023-08-10 17:04:58 +0400

    refactored app component to be single file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  imports: [CommonModule, RouterModule],
+  template: `<router-outlet></router-outlet>`,
+  styles: [``]
 })
 export class AppComponent {
 }
