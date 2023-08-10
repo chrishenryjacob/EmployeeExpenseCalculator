7493f4437dc558d1904eddc67ffaf4943dbaf652
Christopher Henry Jacob <christopherhenryjacob@gmail.com>
2023-08-10 20:05:46 +0400

    added employee service

--- src/app/shared/services/employee/employee.service.ts
+++ src/app/shared/services/employee/employee.service.ts
@@ -1,21 +1,51 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
 import { of } from 'rxjs';
+
+import { StorageService } from '../storage/storage.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  create(payload: any) {
+    const result = this.fetch('EmployeeDetails');
 
-  createEmployee(payload: any) {
-    const storedData = localStorage.getItem('mockData');
-    return of(storedData);
+    const hasDuplicate = result.some((item: any) => item.id === payload.id);
+    if (hasDuplicate) {
+      return of({ isSuccess: false, msg: 'Duplicate value' });
+    }
+
+    result.push(payload);
+    localStorage.setItem('EmployeeDetails', JSON.stringify(result));
+
+    return of({ isSuccess: true, msg: 'Created Successfully' });
   }
 
-  updateEmployee(payload: any) {
-    const storedData = localStorage.getItem('mockData');
-    return of(storedData);
+  read() {
+    return of(this.fetch('EmployeeDetails'))
   }
 
+  readById(id: string) {
+    const result = this.fetch('EmployeeDetails');
+    return of(result.find(item => item.id === id));
+  }
+
+  update(payload: any) {
+    const result = this.fetch('EmployeeDetails');
+    result.map((item: any) => item.id === payload.id ? payload : item);
+    localStorage.setItem('EmployeeDetails', JSON.stringify(result));
+
+    return of({ isSuccess: true, msg: 'Updated Successfully' });
+  }
+
+  delete(id: string) {
+    const result = this.fetch('EmployeeDetails');
+    return of(result.filter(item => item.id !== id));
+  }
+
+  private fetch(name: string): any[] {
+    const data = localStorage.getItem(name);
+    return data ? JSON.parse(data) : []
+  }
 }
