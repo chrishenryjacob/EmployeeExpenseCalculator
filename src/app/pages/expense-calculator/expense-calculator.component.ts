5da22dbfa7e2627a912bbef125a6f6d347adca42
Christopher Henry Jacob <christopherhenryjacob@gmail.com>
2023-08-11 15:33:01 +0400

    dropdown added

--- src/app/pages/expense-calculator/expense-calculator.component.ts
+++ src/app/pages/expense-calculator/expense-calculator.component.ts
@@ -1,13 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { EmployeeService } from '@shared/services/employee/employee.service';
+import { Employee } from '@shared/models/employee.model';
+
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
 
 @Component({
   selector: 'app-expense-calculator',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule, FormsModule,
+    NzCardModule, NzButtonModule, NzSelectModule,NzFormModule
+  ],
   templateUrl: './expense-calculator.component.html',
   styleUrls: ['./expense-calculator.component.scss']
 })
-export class ExpenseCalculatorComponent {
+export class ExpenseCalculatorComponent implements OnInit {
+  employeeService = inject(EmployeeService);
+
+  employeeList: Employee[] = [];
+  selectedData: any[] = [];
+
+  ngOnInit(): void {
+    this.getEmployeeList();
+  }
+
+  getEmployeeList() {
+    this.employeeService.read().subscribe(res =>
+      this.employeeList = res.filter(item => item.type === 'Manager'));
+  }
 
 }
