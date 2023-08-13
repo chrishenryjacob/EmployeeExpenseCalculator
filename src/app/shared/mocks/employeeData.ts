71c896246a6bba7baeb207ffb30b528b8a05227d
Christopher Henry Jacob <christopherhenryjacob@gmail.com>
2023-08-14 00:20:51 +0400

    employee id dependency removal from employee details added

---
+++ src/app/shared/mocks/employeeData.ts
@@ -0,0 +1,12 @@
+export const EmployeeData = [
+    {
+        id: 'employee123',
+        refs: ['ref1', 'ref2'],
+        children: ['id123', 'child2']
+    },
+    {
+        id: 'employee456',
+        refs: ['id123'],
+        children: ['child3']
+    }
+]
--- src/app/shared/services/helper/helper.service.spec.ts
+++ src/app/shared/services/helper/helper.service.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 
 import { HelperService } from './helper.service';
+import { EmployeeData } from '@shared/mocks/employeeData';
 
 describe('HelperService', () => {
   let service: HelperService;
@@ -13,4 +14,14 @@ describe('HelperService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should remove employeeId from refs and children in EmployeeDetails', () => {
+    const data = EmployeeData;
+    spyOn(service, 'fetchFromLocalStorage').and.returnValues(data, []);
+    spyOn(localStorage, 'setItem');
+    service.deleteDependencies('id123');
+    expect(data[0].children).toEqual(['child2']);
+    expect(data[1].refs).toEqual([]);
+  });
+
 });
